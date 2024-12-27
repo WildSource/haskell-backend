c8f6c6b68e8ac8558521e8405a0f7575d1e23763
WildSource <ilovetrap123@hotmail.com>
2024-12-27 14:01:57 -0500

    feat: add Page POST API

--- app/API/Page.hs
+++ app/API/Page.hs
@@ -1,3 +1,12 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeOperators #-}
+
 module API.Page where 
 
+import Servant 
+import Entities.Page (PagesData)
+import Data.Int (Int64)
+
+type PageAPI = 
+  "comics" :> ReqBody '[JSON] PagesData :> Post '[JSON] Int64
 
